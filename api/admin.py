52a3698a21aebee3dc582721bbd652c744cd1cc3
maesterzak <65318935+maesterzak@users.noreply.github.com>
2022-03-31 03:32:30 +0100

    changed admin page

--- api/admin.py
+++ api/admin.py
@@ -2,11 +2,34 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
+
+class CommentsInline(admin.TabularInline):
+    model = Comments
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'views', 'daily_views', 'published_date', 'category')
+    inlines = [
+        CommentsInline,
+    ]
+
+
+class MessageInline(admin.TabularInline):
+    model = Messages
+
+
+class ThreadAdmin(admin.ModelAdmin):
+    list_display = ('title', 'started', 'status')
+    inlines = [
+        MessageInline,
+    ]
+
+
 admin.site.register(Author)
-admin.site.register(Posts)
+admin.site.register(Posts, PostAdmin)
 admin.site.register(Category)
-admin.site.register(Messages)
-admin.site.register(Threads)
-admin.site.register(Comments)
+
+admin.site.register(Threads, ThreadAdmin)
+
 admin.site.register(Portfolio_skills)
 admin.site.register(Portfolio_projects)
